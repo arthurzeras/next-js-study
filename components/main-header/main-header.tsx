5353d20d11aaafe04918df61f04703cfba8f46ee
Arthur Oliveira <arthurolvmorais@brq.com>
2024-03-06 09:46:00 -0300

    refactor: move main header background to a separated component

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -3,25 +3,29 @@ import Image from "next/image";
 
 import imgLogo from "@/assets/logo.png";
 import styles from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
 export default () => {
   return (
-    <header className={styles.header}>
-      <Link className={styles.logo} href="/">
-        <Image src={imgLogo} alt="A plate with food on it" priority />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={imgLogo} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
