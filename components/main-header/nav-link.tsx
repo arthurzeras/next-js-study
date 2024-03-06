2f3f33923ec585c5c5e5ceba5abaad6da9287118
Arthur Oliveira <arthurolvmorais@brq.com>
2024-03-06 10:21:16 -0300

    feat: implement dynamic class name change for links in each page

---
+++ components/main-header/nav-link.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import styles from "./nav-link.module.css";
+
+interface Props {
+  href: string;
+}
+
+export default ({ href, children }: React.PropsWithChildren<Props>) => {
+  const path = usePathname();
+  let className = styles["nav-link"];
+
+  if (path.startsWith(href)) {
+    className += ` ${styles["active"]}`;
+  }
+
+  return (
+    <Link href={href} className={className}>
+      {children}
+    </Link>
+  );
+};
